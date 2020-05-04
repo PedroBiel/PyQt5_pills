34864008772b7af14f805a940728ffc02f0dad8b
Pedro Biel <structural.eng.biel@gmail.com>
2020-05-04 17:36:52 +0200

    Add files via upload

---
+++ MainWindow_calls_Dialog/dialog_MVC/data/initialdialog.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+PyQt5 pills
+Mainwindow calls Dialog
+
+Created on 04.05.2020
+
+__author__ = Pedro Biel
+__version__ = 0.0.0
+__email__ = structural.eng.biel@gmail.com
+"""
+
+
+class InitialDataDialog:
+    """Initial data of the dialog."""
+    
+    DATA = {
+        'input1': '123',
+        'input2': 'abc'
+        }
