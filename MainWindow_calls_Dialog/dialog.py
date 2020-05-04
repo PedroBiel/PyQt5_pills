836c6b87e935bcce6d077f064f4c2b60995f24ed
Pedro Biel <structural.eng.biel@gmail.com>
2020-05-04 17:33:36 +0200

    Add files via upload

---
+++ MainWindow_calls_Dialog/dialog.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+PyQt5 pills
+Mainwindow calls Dialog
+
+Created on 04.05.2020
+
+__author__ = Pedro Biel
+__version__ = 0.0.0
+__email__ = structural.eng.biel@gmail.com
+"""
+
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QDialog
+
+from dialog_MVC.controller.cnt_dialog import CntDialog
+
+
+class Dialog(QDialog):
+    
+    def __init__(self, parent=None):
+        """Dialog of the application."""
+        
+        QDialog.__init__(self, parent)
+        uic.loadUi('view/dialog.ui', self)
+        
+        # Objects of the application.
+        self.d_dlg_data = {}
+        
+        # Instances of the classes.
+        self.cnt_dialog = CntDialog(self)
+        
+        # PyQt5 objects
+        self.dlg_input1 = self.lineEditInput1
+        self.dlg_input2 = self.lineEditInput2
+        self.btn_apply = self.pushButtonApply
+        self.btn_cancel = self.pushButtonCancel
+        
+        # Add items to QLineEdit.
+        self.d_dlg_data = self.cnt_dialog.dialog_initial_data()
+        self.dlg_input1.setText(self.d_dlg_data['input1'])
+        self.dlg_input1.selectAll()
+        self.dlg_input2.setText(self.d_dlg_data['input2'])
+        
+        # Events.
+        self.btn_apply.clicked.connect(self.cnt_dialog.dialog_data)
+        self.btn_apply.clicked.connect(self.reject)
+        self.btn_cancel.clicked.connect(self.close)
+        
+    def d_dialog_data(self):
+        return self.d_dlg_data
---
+++ MainWindow_calls_Dialog/run.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+PyQt5 pills
+Mainwindow calls Dialog
+
+Created on 04.05.2020
+
+__author__ = Pedro Biel
+__version__ = 0.0.0
+__email__ = structural.eng.biel@gmail.com
+"""
+
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from dialog import Dialog
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, parent=None):
+        """Run the MainWindow and calls the Dialog."""
+
+        QMainWindow.__init__(self, parent)
+        uic.loadUi('view/mainwindow.ui', self)
+        
+        # App objects.
+        self.d_data = {}
+        
+        # PyQt5 objects.
+        self.btn_call_dialog = self.pushButtonCallDialog
+        self.btn_refresh = self.pushButtonRefresh
+        self.input1 = self.labelInput1
+        self.input2 = self.labelInput2
+        
+        # Instances of the classes.
+        self.dialog = Dialog(self)
+        
+        # Events.
+        self.btn_call_dialog.clicked.connect(self.call_dialog)
+        self.btn_refresh.clicked.connect(self.get_dialog_inputs)
+    
+    def call_dialog(self):
+        self.dialog.show()
+        self.d_data = self.dialog.d_dialog_data()
+        # self.get_dialog_inputs()
+        
+    def get_dialog_inputs(self):
+        dlg = self.dialog
+        dlg_data = dlg.d_dialog_data()
+        self.input1.setText(dlg_data['input1'])
+        self.input2.setText(dlg_data['input2'])
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myapp = MainWindow()
+    myapp.show()
+    sys.exit(app.exec_())
