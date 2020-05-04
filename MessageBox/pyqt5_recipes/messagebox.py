e907f35f7f85b43ac65d3e95f60ea91c90fe5f3a
Pedro Biel <structural.eng.biel@gmail.com>
2020-05-04 19:53:40 +0200

    Add files via upload

---
+++ MessageBox/pyqt5_recipes/messagebox.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+PyQt5 pills
+MessageBox
+
+Created on 04.05.2020
+
+__author__ = Pedro Biel
+__version__ = 0.0.0
+__email__ = structural.eng.biel@gmail.com
+"""
+
+
+from PyQt5.QtWidgets import QMessageBox
+
+
+class MessageBox:
+    
+    def __init__(self, window_title, text, info_text, detail_text=''):
+        """
+        Display informational messages.
+        
+        setStandardButtons():   
+            QMessageBox.Ok
+            QMessageBox.Open
+            QMessageBox.Save
+            QMessageBox.Cancel
+            QMessageBox.Close
+            QMessageBox.Yes
+            QMessageBox.No
+            QMessageBox.Abort
+            QMessageBox.Retry
+            QMessageBox.Ignore
+        e.g.: msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        """
+        self.window_title = window_title
+        self.text = text
+        self.info_text = info_text
+        self.detail_text = detail_text
+        
+        self.msg = QMessageBox()
+        
+    def critical(self):
+        self.msg.setIcon(QMessageBox.Critical)
+        self.msg.setWindowTitle(self.window_title)
+        self.msg.setText(self.text)
+        self.msg.setInformativeText(self.info_text)
+        self.msg.setStandardButtons(QMessageBox.Ok)
+        self.msg.exec_()
+    
+    def information(self):
+        self.msg.setIcon(QMessageBox.Information)
+        self.msg.setWindowTitle(self.window_title)
+        self.msg.setText(self.text)
+        self.msg.setInformativeText(self.info_text)
+        self.msg.setStandardButtons(QMessageBox.Ok)
+        self.msg.exec_()
+    
+    def question(self):
+        self.msg.setIcon(QMessageBox.Question)
+        self.msg.setWindowTitle(self.window_title)
+        self.msg.setText(self.text)
+        self.msg.setInformativeText(self.info_text)
+        self.msg.setStandardButtons(QMessageBox.Ok)
+        self.msg.exec_()
+    
+    def warning(self):
+        self.msg.setIcon(QMessageBox.Warning)
+        self.msg.setWindowTitle(self.window_title)
+        self.msg.setText(self.text)
+        self.msg.setInformativeText(self.info_text)
+        self.msg.setStandardButtons(QMessageBox.Ok)
+        self.msg.exec_()
+    
+    def critical_detailed_text(self):
+        self.msg.setIcon(QMessageBox.Critical)
+        self.msg.setWindowTitle(self.window_title)
+        self.msg.setText(self.text)
+        self.msg.setInformativeText(self.info_text)
+        self.msg.setDetailedText(self.detail_text)
+        self.msg.setStandardButtons(QMessageBox.Ok)
+        self.msg.exec_()
+    
+    def information_detailed_text(self):
+        self.msg.setIcon(QMessageBox.Information)
+        self.msg.setWindowTitle(self.window_title)
+        self.msg.setText(self.text)
+        self.msg.setInformativeText(self.info_text)
+        self.msg.setDetailedText(self.detail_text)
+        self.msg.setStandardButtons(QMessageBox.Ok)
+        self.msg.exec_()
+    
+    def question_detailed_text(self):
+        self.msg.setIcon(QMessageBox.Question)
+        self.msg.setWindowTitle(self.window_title)
+        self.msg.setText(self.text)
+        self.msg.setInformativeText(self.info_text)
+        self.msg.setDetailedText(self.detail_text)
+        self.msg.setStandardButtons(QMessageBox.Ok)
+        self.msg.exec_()
+    
+    def warning_detailed_text(self):
+        self.msg.setIcon(QMessageBox.Warning)
+        self.msg.setWindowTitle(self.window_title)
+        self.msg.setText(self.text)
+        self.msg.setInformativeText(self.info_text)
+        self.msg.setDetailedText(self.detail_text)
+        self.msg.setStandardButtons(QMessageBox.Ok)
+        self.msg.exec_()
+        
